e95fd121731bf2750cc59a5023ac06714f0e9b93
slaxeea <kappelers77@gmail.com>
2022-04-27 13:41:24 +0200

    added the main form

--- m306-finanzplaner/src/components/HelloWorld.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Finanzplaner</h1>
-    <p>Wie lange brauche ich um finanziell Unabhängig zu sein?</p>
-  </div>
-
-</template>
---
+++ m306-finanzplaner/src/components/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Finanzplaner</h1>
+    <p>Wie lange brauche ich um finanziell Unabhängig zu sein?</p>
+    <hr />
+    <div class="main-form">
+      <form>
+        <div class="form-group">
+          <label for="current_wealth">Aktuelles Kapital</label>
+          <input id="current_wealth" v-model="current_wealth" />
+        </div>
+        <div class="form-group">
+          <label for="current_income">Aktuelles Einkommen</label>
+          <input id="current_income" v-model="current_income" />
+        </div>
+        <div class="form-group">
+          <label for="current_expense">Aktuelle Ausgaben</label>
+          <input id="current_expense" v-model="current_expense" />
+        </div>
+        <div class="form-group">
+          <label for="current_montly_deposit"
+            >Aktuelle monatliche Einzahlungen</label
+          >
+          <input id="current_montly_deposit" v-model="current_montly_deposit" />
+        </div>
+        <div class="form-group">
+          <label for="current_stockvalue">Aktuelles Aktienkapital</label>
+          <input id="current_stockvalue" v-model="current_stockvalue" />
+        </div>
+        <div class="form-group">
+          <label for="expected_yearly_dividends_percent"
+            >Erwartete jährliche Dividenden</label
+          >
+          <input
+            id="expected_yearly_dividends_percent"
+            v-model="expected_yearly_dividends_percent"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      current_wealth: 0,
+      current_income: 0,
+      current_expense: 0,
+      current_monthly_deposit: 0,
+      current_stockvalue: 0,
+      expected_yearly_dividends_percent: 0,
+    };
+  },
+  methods: {},
+};
+</script>
+
+
+<style scoped>
+.form-group {
+  margin-bottom: 1rem;
+}
+.main-form {
+  margin-top: 2rem;
+}
+</style>
